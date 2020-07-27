448d2dcc66b62178a30085dbd7116aa64d6b4bff
PennWarren <51812919+PennWarren@users.noreply.github.com>
2020-07-26 22:36:35 -0400

    Environment ready for non-neural network style reinforcement learning -- Consolidated repetitive classes -- Much cleaner (and commented) code

--- Scenarios/Simple.py
+++ Scenarios/Simple.py
@@ -1,13 +1,14 @@
 """
 Written by Benjamin Raiford
 Modeled after multiagent-particle-envs (MPE) from OpenAI
-7/19/2020
+
+Initial Action class and reward function written by Frank Liu
+
+Updated 7/26/20 by Ben Raiford
 """
 
 import numpy as np
 
-
-# modified by Frank Liu for Action class and reward function
 
 class Agent:
     # Init method
@@ -23,82 +24,69 @@ class Agent:
 
 class Action:
     def __init__(self):
-        action_space = ['left', 'right', 'up', 'down', 'stay']
-        self.action = action_space[-1]
-
-    def move(self, agent, world):
-        current_column = agent.column
-        current_row = agent.row
-
-        # Left
-        if agent.action == 'left':
-            if not world.line[current_row, current_column - 1]:
-                agent.column = agent.column - 1
-        # Right
-        elif agent.action == 'right':
-            if not world.line[current_row, current_column + 1]:
-                agent.column = agent.column + 1
-        # Up
-        elif agent.action == 'up':
-            if not world.line[current_row - 1, current_column]:
-                agent.row = agent.row - 1
-        # Down
-        elif agent.action == 'down':
-            if not world.line[current_row + 1, current_column]:
-                agent.row = agent.row + 1
-        # Any code for stay would be redundant
-
-    def act(self, world):
-        # FIXME
-        for agent in enumerate(world.agents):
-            agent.action.move(agent, world)
-            world.timestep += 1
+        """
+        Possible directions
+            0: Stay
+            1: Left
+            2: Right
+            3: Up
+            4: Down
+        """
+        self.direction = -1
+        # Communication (for potential later implementation)
+        # NOT IMPLEMENTED
+        self.communication = False
 
 
 class World:
     # Init method
     def __init__(self, n_agents):
+        # Set num_agents and declare agents array
         self.num_agents = n_agents
         self.agents = []
-        self.line = np.empty((3, n_agents), Agent)
-        self.timestep = 0
 
-    # FIXME: next need to build step to make algorithms work
-    def step(self):
-        for agent in self.agents:
-            agent.action = 'stay'
+        # Create array environment for agents to order themselves in
+        self.height = 3
+        self.width = n_agents
+        self.line = np.empty((self.height, self.width), Agent)
 
-class Scenario:
-    def make_world(self, n_agents=3):
-        world = World(n_agents)
-        world.agents = [Agent() for i in range(n_agents)]
-        for i, agent in enumerate(world.agents):
+        # Global time counter
+        self.timestep = -1
+
+    # Makes world (for use at the beginning)
+    def make_world(self):
+        # Create agents to fill the agents array
+        self.agents = [Agent() for i in range(self.num_agents)]
+        for i, agent in enumerate(self.agents):
             agent.name = 'Agent {}'.format(i)
             agent.sight = 'others'
 
-        self.reset_world(world, n_agents)
-        return world
+        self.reset_world()
+        return self
 
-    def reset_world(self, world, n_agents):
+    # Resets the world (for use after every episode)
+    def reset_world(self):
+        # Reset the time step
+        self.timestep = 0
 
-        world.timestep = 0
-
-        for i, agent in enumerate(world.agents):
+        for i, agent in enumerate(self.agents):
+            # Assign each agent a random number
             agent.number = np.random.randint(1, 100, dtype=int)
+            # Set each agent's location to its initial position
             agent.column = i
             agent.row = 1
 
-            world.line[agent.row, agent.column] = agent
+            # Update world with agents initial location
+            self.line[agent.row, agent.column] = agent
 
-        line_numbers = [a.number for a in world.line[1, ]]
-        print("DEBUG: line_numbers:", line_numbers)
+        self.debug_print_line()
 
-    def check_done(self, world):
+    def check_done(self):
         done = True
         filled = True
 
-        for i in range(world.num_agents - 1):
-            if not world.line[1, i]:
+        for i in range(self.num_agents):
+            if not self.line[1, i]:
                 print("The line is not filled")
                 filled = False
                 done = False
@@ -107,18 +95,112 @@ class Scenario:
                 break
 
         if filled:
-            for i in range(world.num_agents - 1):
-                if world.line[1, i].number > world.line[1, i + 1].number:
+            for i in range(self.num_agents - 1):
+                if self.line[1, i].number > self.line[1, i + 1].number:
                     done = False
 
         return done
 
-    def reward(self, agent, world):
-        return world.timestep * -1
+    def debug_print_line(self, show_row=1):
+        line_numbers = [a.number for a in self.line[1,]]
+        print("DEBUG: line_numbers:", line_numbers)
 
+    # Agent movement mechanic
+    def move(self, agent):
+
+        original_column = agent.column
+        original_row = agent.row
+
+        # Stay
+        if agent.action.direction == 0:
+            return
+
+        # Left
+        if agent.action.direction == 1:
+            # If moving agent is not in the left-most column
+            if agent.column is not 0:
+                # If space is not occupied
+                if not self.line[original_row, original_column - 1]:
+                    # Change agent's location and clear the original location
+                    agent.column = agent.column - 1
+                    self.line[original_row, original_column] = None
+
+        # Right
+        elif agent.action.direction == 2:
+            # If moving agent is not in right-most column
+            if agent.column is not self.num_agents - 1:
+                # If space is not occupied
+                if not self.line[original_row, original_column + 1]:
+                    # Change agent's location and clear the original location
+                    agent.column = agent.column + 1
+                    self.line[original_row, original_column] = None
+
+        # Up
+        elif agent.action.direction == 3:
+            # If moving agent is not in top row
+            if agent.row is not 0:
+                # If space is not occupied
+                if not self.line[original_row - 1, original_column]:
+                    # Change agent's location and clear the original location
+                    agent.row = agent.row - 1
+                    self.line[original_row, original_column] = None
+
+        # Down
+        elif agent.action.direction == 4:
+            # If moving agent is not in bottom row
+            if agent.row is not 2:
+                # If space is not occupied
+                if not self.line[original_row + 1, original_column]:
+                    # Change agent's location and clear the original location
+                    agent.row = agent.row + 1
+                    self.line[original_row, original_column] = None
+
+        # Update world with agent's new location
+        self.line[agent.row, agent.column] = agent
+
+    # Step function
+    def step(self, agent_actions):
+        # Assert that there is an action for every agent (and no more)
+        assert (len(agent_actions) == self.num_agents)
+
+        # Initialize reward
+        reward = 0
+
+        # For every agent
+        for i, agent in enumerate(self.agents):
+            # Get agent's action from the array
+            agent.action.direction = agent_actions[i]
+            # Move
+            self.move(agent)
+            # DEBUG: Print current array
+            print(self.line, "\n")
+
+        # Check if the environment is done
+        done = self.check_done()
+
+        # Time step and negative reward
+        # FIXME: make sure this shouldn't happen on the last iteration as well
+        if not done:
+            self.timestep += 1
+            reward = -1
+
+        # Return reward and done
+        return reward, done
 
 # Entry point
 if __name__ == "__main__":
-    scen = Scenario()
-    env = scen.make_world(2)
-    scen.check_done(env)
+    # set parameters
+    num_agents = 2
+
+    # initialize environment
+    env = World(num_agents)
+    env.make_world()
+
+    # declare policy array
+    policy = np.zeros(num_agents)
+    # fill policy array with random
+    for i in range(num_agents):
+        policy[i] = np.random.randint(5)
+
+    print(policy)
+    print(env.step(policy))
